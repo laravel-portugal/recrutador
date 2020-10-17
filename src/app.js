882a0bf796c7e5756532860acbe7e1d56b01cb70
Michel Melo <michelmelo@users.noreply.github.com>
2020-10-17 17:30:34 +0100

    Versão funcional 0.0.1 (#4)
    * Added drivers fro ItJobs, LandindJobs
    
    * Added better NameSapce
    
    * Rename bot.py to main.py
    
    * Add base class getEnvFilePath()
    
    * Added error catching
    
    * Add more error catching code, and autorestart in case client error
    
    * Better request logic
    
    * Fix
    
    * Add --dry argument
    
    * Check if api_key is set
    
    * Make tags mandatory
    
    * Added .env keys to readme
    
    * init
    
    * add discord.js
    
    * clear format
    
    * Create .env.example
    
    * Remove nonrequired dependencies
    
    * Add Optional packages
    
    * Add dotenv
    
    * Working version
    
    * Making request easier
    
    * Base Driver
    
    * Pause execution
    
    * LandingJobs driver first version
    
    * Add job processing and execution pause
    
    * Cleanning up
    
    * More cleaning up
    
    * Update readme
    
    * ItJobs
    
    * Create node.js.yml
    
    Co-authored-by: Jorge Costa <jorgercosta@gmail.com>
    Co-authored-by: Michel Melo <michel.melo@servdebt.pt>

---
+++ src/app.js
@@ -0,0 +1,73 @@
+//get environment variables from .env
+require('dotenv').config()
+const Discord = require('discord.js');
+const sleep = require('./sleep');
+const LandingJobs = require('./drivers/LandingJobs');
+const ItJobs = require('./drivers/ItJobs');
+
+
+//create a new discord client
+const client = new Discord.Client();
+
+/**
+ * called when client is ready
+ */
+client.on('ready', async () => {
+    console.log(`Ready to process jobs!`);
+    do {
+        await processJobs()
+    } while (true);
+});
+
+//called when a error occurs in discord client
+client.on('shardError', error => {
+    console.error('A websocket connection encountered an error:', error);
+    console.log('Restarting script');
+    client.destroy()
+    client.login(process.env.TOKEN);
+});
+
+/**
+ * iterates drivers array and posts jobs on the specified channel
+ * @returns {void}
+ */
+async function processJobs() {
+    try {
+        for (let index = 0; index < drivers.length; index++) {
+
+            //awaits jobs from api
+            let jobs = await drivers[index].getJobs();
+
+            for (let jobIndex = 0; jobIndex < jobs.length; jobIndex++) {
+
+                //get the channel
+                const channel = client.channels.cache.get(process.env.CHANNELID);
+
+                //logs the job url
+                console.info("Sending job url: " + jobs[jobIndex].url)
+
+                //awaits publishing job
+                // await channel.send(jobs[jobIndex].url)
+
+                //stores the job in config file
+                // drivers[index].storePublishedJob(jobs[jobIndex])
+            }
+        }
+        //pauses execution 
+        console.info("Sleeping for " + parseInt(process.env.FETCHINTERVAL) * 1000)
+        await sleep(parseInt(process.env.FETCHINTERVAL) * 1000)
+
+
+    } catch (error) {
+        console.error("processJobs -> error", error)
+    }
+
+}
+
+
+//register each driver
+//itJobs missing
+const drivers = [new LandingJobs(), new ItJobs()]
+
+//login with token from .env
+client.login(process.env.TOKEN);
